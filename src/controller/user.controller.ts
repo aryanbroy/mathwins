d7b3b5ec7b879e94adbc521a554d5cbfc8a72ac5
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-10-30 01:12:49 +0530

    merge conflict commit-1

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from 'express';
 import prisma from '../prisma';
 
-export const getAllUsers = (req: Request, res: Response) => {
-  res.status(200).json({ failure: false });
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (err) {
+    console.log('Error occurred: ', err);
+    res.status(400).json(err);
+  }
 };
 
 export const createUser = async (req: Request, res: Response) => {
   try {
-    const {name, email, password} = req.body;
+    const { name, email, password } = req.body;
     const user = await prisma.user.create({
       data: {
         name,
