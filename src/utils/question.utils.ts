72ec780e12284dda54c6024e7e88cedaf176f2ad
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2025-11-02 16:28:40 +0530

    rename createQuestions to generateQuestion in utils/question.utils.ts

--- src/utils/question.utils.ts
+++ src/utils/question.utils.ts
@@ -9,55 +9,8 @@ function shuffle(array: any[]): any[] {
 }
 export const generateQuestions = async (size: number) => {
   try {
-    const totalQuestions = size;
-    const targetCounts = {
-      EASY: Math.floor(totalQuestions * 0.4),
-      MEDIUM: Math.floor(totalQuestions * 0.3),
-      HARD: Math.floor(totalQuestions * 0.3),
-    };
-    const remainder =
-      totalQuestions -
-      (targetCounts.EASY + targetCounts.MEDIUM + targetCounts.HARD);
-    targetCounts.EASY += remainder;
-    const [easyQuestions, mediumQuestions, hardQuestions] =
-      await prisma.$transaction([
-        prisma.question.findMany({
-          where: { difficulty: 1 },
-        }),
-        prisma.question.findMany({
-          where: { difficulty: 2 },
-        }),
-        prisma.question.findMany({
-          where: { difficulty: 3 },
-        }),
-      ]);
-
-    const available = {
-      EASY: shuffle(easyQuestions),
-      MEDIUM: shuffle(mediumQuestions),
-      HARD: shuffle(hardQuestions),
-    };
-    const selectedQuestions = [];
-    const easyPicks = available.EASY.splice(0, targetCounts.EASY);
-    selectedQuestions.push(...easyPicks);
-
-    const mediumPicks = available.MEDIUM.splice(0, targetCounts.MEDIUM);
-    selectedQuestions.push(...mediumPicks);
-
-    const hardPicks = available.HARD.splice(0, targetCounts.HARD);
-    selectedQuestions.push(...hardPicks);
-
-    let shortfall = totalQuestions - selectedQuestions.length;
-    if (shortfall > 0) {
-      const fallbackPool = [
-        ...available.EASY,
-        ...available.MEDIUM,
-        ...available.HARD,
-      ];
-      const fallbackPicks = fallbackPool.splice(0, shortfall);
-      selectedQuestions.push(...fallbackPicks);
-    }
-    return selectedQuestions;
+    console.log("try");
+    
   } catch (err) {
     console.log('Error occurred: ', err);
     throw new Error('Error Occured !!');
