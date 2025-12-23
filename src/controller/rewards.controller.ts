0f49a7ec314217901a73f84dc918e03fe0a70b62
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-12-23 22:11:47 +0530

    list claims made by user

--- src/controller/rewards.controller.ts
+++ src/controller/rewards.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import { asyncHandler } from '../middlewares/asyncHandler';
 import { ApiError } from '../utils/api/ApiError';
-import { postClaimRequestHandler } from '../helpers/reward.helper';
+import {
+  listRewardClaimsHandler,
+  postClaimRequestHandler,
+} from '../helpers/reward.helper';
 import prisma from '../prisma';
 import { ApiResponse } from '../utils/api/ApiResponse';
 
@@ -25,3 +28,17 @@ export const postClaimRequest = asyncHandler(
       .json(new ApiResponse(200, result, 'sent a claim request to the admins'));
   }
 );
+
+export const listClaims = asyncHandler(async (req: Request, res: Response) => {
+  const { userId } = req;
+  if (!userId) {
+    throw new ApiError({
+      statusCode: 400,
+      message: 'Received invalid user id from auth handler',
+    });
+  }
+
+  const claims = await listRewardClaimsHandler(userId);
+
+  res.status(200).json(new ApiResponse(200, claims, 'fetched user claims'));
+});
--- src/helpers/reward.helper.ts
+++ src/helpers/reward.helper.ts
@@ -1,5 +1,7 @@
 import { Prisma } from '../generated/prisma';
+import prisma from '../prisma';
 import { ApiError } from '../utils/api/ApiError';
+import { CLAIM_STATUS_MAP } from '../utils/user.util';
 
 export const postClaimRequestHandler = async (
   tx: Prisma.TransactionClient,
@@ -61,4 +63,83 @@ export const postClaimRequestHandler = async (
     }
     throw e;
   }
+};
+
+type BaseHistory = {
+  id: string;
+  status: string;
+  coinsLocked: number;
+  createdAt: Date;
+};
+
+type FulfilledHistory = {
+  id: string;
+  status: string;
+  coinsLocked: number;
+  createdAt: Date;
+  voucherCode: string;
+};
+
+type RejectedHistory = {
+  id: string;
+  status: string;
+  coinsLocked: number;
+  createdAt: Date;
+  rejectionReason: string;
+};
+
+type ClaimHistory = BaseHistory | FulfilledHistory | RejectedHistory;
+
+export const listRewardClaimsHandler = async (userId: string) => {
+  const claimHistory = await prisma.rewardClaim.findMany({
+    where: { userId },
+    select: {
+      status: true,
+      coinsLocked: true,
+      voucherCode: true,
+      rejectionReason: true,
+      adminNotes: true,
+      createdAt: true,
+      id: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  const history: ClaimHistory[] = claimHistory.map((claim) => {
+    if (claim.status === 'PENDING') {
+      return {
+        id: claim.id,
+        status: CLAIM_STATUS_MAP[claim.status],
+        coinsLocked: claim.coinsLocked,
+        createdAt: claim.createdAt,
+      };
+    } else if (claim.status === 'FULFILLED') {
+      return {
+        id: claim.id,
+        status: CLAIM_STATUS_MAP[claim.status],
+        coinsLocked: claim.coinsLocked,
+        createdAt: claim.createdAt,
+        voucherCode: claim.voucherCode!,
+      };
+    } else if (claim.status === 'REJECTED') {
+      return {
+        id: claim.id,
+        status: CLAIM_STATUS_MAP[claim.status],
+        coinsLocked: claim.coinsLocked,
+        createdAt: claim.createdAt,
+        rejectionReason: claim.rejectionReason!,
+      };
+    }
+
+    return {
+      id: claim.id,
+      status: CLAIM_STATUS_MAP[claim.status] || claim.status,
+      coinsLocked: claim.coinsLocked,
+      createdAt: claim.createdAt,
+    };
+  });
+
+  return history;
 };
